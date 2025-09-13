9bf3cd12cea9df97b54bdfdbb4969f8d7b887b91
Harshit_repo <156446920+HarshitTest26@users.noreply.github.com>
2025-09-14 01:08:24 +0530

    Update run_tests.bat

--- run_tests.bat
+++ run_tests.bat
@@ -1,4 +1,11 @@
 @echo off
-pip install --upgrade pytest-html-reporter
-pytest -v -s testCases\test_AccountRegistration_01.py
+rem Uninstall the old, incompatible plugin
+pip uninstall pytest-html-reporter -y
+
+rem Install the correct and compatible plugin
+pip install pytest-html
+
+rem Run the tests and generate the report
+pytest -v -s --html="reports\report.html" testCases\test_AccountRegistration_01.py
+
 pause
